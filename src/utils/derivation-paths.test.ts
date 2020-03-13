14dd5a75caf5206f5f23fc81342c3e48eef51e93
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-03-13 14:51:23 +0100

    Fix tests (#1)

---
+++ src/utils/derivation-paths.test.ts
@@ -0,0 +1,15 @@
+import { HARDENED_OFFSET } from '../constants';
+import { getIndex } from './derivation-paths';
+
+describe('getIndex', () => {
+  it('returns the index for a derivation path level', () => {
+    expect(getIndex(`0`)).toBe(0);
+    expect(getIndex(`1'`)).toBe(HARDENED_OFFSET + 1);
+    expect(getIndex(`12345`)).toBe(12345);
+    expect(getIndex(`12345'`)).toBe(HARDENED_OFFSET + 12345);
+  });
+
+  it('throws if the path is invalid', () => {
+    expect(() => getIndex('foo')).toThrow();
+  });
+});
