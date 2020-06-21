966a742a7da048693afde11018b2beb96097aa8c
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-06-21 02:26:45 +0200

    Refactor secp256k1 implementation to a functional approach

--- src/secp256k1/point.ts
+++ src/secp256k1/point.ts
@@ -1,113 +1,131 @@
 import { bigIntToBuffer, bufferToBigInt } from '../utils';
 import { Curve } from './curve';
-import { divide, mod, multiply, subtract } from './mod';
+import { divide, mod, multiply as modMultiply, subtract } from './mod';
 
-export class Point {
-  constructor(readonly curve: Curve, readonly x: bigint, readonly y: bigint) {}
+/**
+ * A point on an elliptic curve.
+ */
+export interface Point {
+  readonly x: bigint;
+  readonly y: bigint;
+}
 
-  /**
-   * Add another point to this point.
-   *
-   * @param {Point} right
-   * @return {Point}
-   */
-  add(right: Point): Point {
-    if (this.equals(right)) {
-      return this.double();
-    }
-
-    const ys = subtract(this.curve, right.y, this.y);
-    const xs = subtract(this.curve, right.x, this.x);
-    const s = divide(this.curve, ys, xs);
-
-    const x = subtract(this.curve, subtract(this.curve, multiply(this.curve, s, s), this.x), right.x);
-    const y = subtract(this.curve, multiply(this.curve, s, subtract(this.curve, this.x, x)), this.y);
-
-    return new Point(this.curve, x, y);
+/**
+ * Add two points together. Returns a new point.
+ *
+ * @param {Curve} curve
+ * @param {Point} left
+ * @param {Point} right
+ * @return {Point}
+ */
+export const add = (curve: Curve, left: Point, right: Point): Point => {
+  if (isEqual(left, right)) {
+    return double(curve, left);
   }
 
-  /**
-   * Double the point.
-   *
-   * @return {Point}
-   */
-  double(): Point {
-    const sn = this.x ** 2n * 3n + this.curve.a;
-    const sd = 2n * this.y;
-    const s = divide(this.curve, sn, sd);
+  const ys = subtract(curve, right.y, left.y);
+  const xs = subtract(curve, right.x, left.x);
+  const s = divide(curve, ys, xs);
 
-    const x = mod(this.curve, s ** 2n - this.x * 2n);
-    const y = mod(this.curve, s * (this.x - x) - this.y);
+  const x = subtract(curve, subtract(curve, modMultiply(curve, s, s), left.x), right.x);
+  const y = subtract(curve, modMultiply(curve, s, subtract(curve, left.x, x)), left.y);
 
-    return new Point(this.curve, x, y);
+  return {
+    x,
+    y
+  };
+};
+
+/**
+ * Doubles the point provided. Returns a new point.
+ *
+ * @param {Curve} curve
+ * @param {Point} point
+ * @return {Point}
+ */
+export const double = (curve: Curve, point: Point): Point => {
+  const sn = point.x ** 2n * 3n + curve.a;
+  const sd = 2n * point.y;
+  const s = divide(curve, sn, sd);
+
+  const x = mod(curve, s ** 2n - point.x * 2n);
+  const y = mod(curve, s * (point.x - x) - point.y);
+
+  return {
+    x,
+    y
+  };
+};
+
+/**
+ * Multiply a point by a bigint or Buffer. If the value is `1`, the same point will be returned. Otherwise, it returns a
+ * new point.
+ *
+ * @param {Curve} curve
+ * @param {Point} point
+ * @param {Buffer | bigint} value
+ */
+export const multiply = (curve: Curve, point: Point, value: Buffer | bigint): Point => {
+  if (typeof value !== 'bigint') {
+    value = bufferToBigInt(value);
   }
 
-  /**
-   * Multiply the point by a bigint or Buffer. If the value is `1`, the same point will be returned.
-   *
-   * @param {Buffer | bigint} value
-   * @return {Point}
-   */
-  multiply(value: Buffer | bigint): Point {
-    if (typeof value !== 'bigint') {
-      value = bufferToBigInt(value);
-    }
-
-    if (value === 1n) {
-      return this;
-    }
-
-    const s = value.toString(2);
-    const binaryLength = s.length - 1;
-
-    const addings: Point[] = [];
-    let n: Point = this; // eslint-disable-line @typescript-eslint/no-this-alias
-    for (let i = binaryLength; i >= 0; i--) {
-      const char = s[i];
-      if (char === '1') {
-        addings.push(n);
-      }
-
-      n = n.double();
-    }
-
-    let point = addings[0];
-    addings.shift();
-    while (addings[0]) {
-      point = point.add(addings[0]);
-      addings.shift();
-    }
-
+  if (value === 1n) {
     return point;
   }
 
-  /**
-   * Get the point as a SEC1 encoded Buffer.
-   *
-   * @param {boolean} compact
-   * @return {Buffer}
-   */
-  toBuffer(compact = false): Buffer {
-    const length = Buffer.from(this.curve.p.toString(16), 'hex').byteLength;
-    const x = bigIntToBuffer(this.x, length);
+  const s = value.toString(2);
+  const binaryLength = s.length - 1;
 
-    if (compact) {
-      const isEven = this.y % 2n === 0n;
-      return Buffer.concat([Buffer.from([isEven ? 0x02 : 0x03]), x]);
+  const addings: Point[] = [];
+  for (let i = binaryLength; i >= 0; i--) {
+    const char = s[i];
+    if (char === '1') {
+      addings.push(point);
     }
 
-    const y = bigIntToBuffer(this.y, length);
-
-    return Buffer.concat([Buffer.from([0x04]), x, y]);
+    point = double(curve, point);
   }
 
-  /**
-   * Check if the point is equal to another point.
-   *
-   * @param {Point} point
-   * @return {boolean}
-   */
-  private equals(point: Point): boolean {
-    return this.x === point.x && this.y === point.y;
+  let newPoint = addings[0];
+  addings.shift();
+  while (addings[0]) {
+    newPoint = add(curve, newPoint, addings[0]);
+    addings.shift();
   }
-}
+
+  return newPoint;
+};
+
+/**
+ * Get a point as a SEC1 encoded Buffer.
+ *
+ * @param {Curve} curve
+ * @param {Point} point
+ * @param {boolean} compact
+ * @return {Buffer}
+ */
+export const toBuffer = (curve: Curve, point: Point, compact = false): Buffer => {
+  const length = Buffer.from(curve.p.toString(16), 'hex').byteLength;
+  const x = bigIntToBuffer(point.x, length);
+
+  if (compact) {
+    const isEven = point.y % 2n === 0n;
+    return Buffer.concat([Buffer.from([isEven ? 0x02 : 0x03]), x]);
+  }
+
+  const y = bigIntToBuffer(point.y, length);
+
+  return Buffer.concat([Buffer.from([0x04]), x, y]);
+};
+
+/**
+ * Check if two points are equal.
+ *
+ * @param {Point} left
+ * @param {Point} right
+ * @return {boolean}
+ */
+export const isEqual = (left: Point, right: Point): boolean => {
+  return left.x === right.x && left.y === right.y;
+};
