b7148a83bde7d686e10af80c3a6d5555bd2bfd5c
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-10-14 17:05:19 +0200

    Use browser compatible Buffer library

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { Buffer } from 'buffer';
+
 export const MASTER_KEY = Buffer.from('Bitcoin seed', 'utf8');
 export const PUBLIC_KEY_VERSION = 0x0488b21e;
 export const PRIVATE_KEY_VERSION = 0x0488ade4;
--- src/hdnode.test.ts
+++ src/hdnode.test.ts
@@ -1,3 +1,4 @@
+import { Buffer } from 'buffer';
 import { HDNode } from './hdnode';
 
 interface TestVector {
