d35b6ec9e5826b8b4d38f1ad487110b60d2275f6
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-01-25 01:10:45 +0100

    Add @findeth/abi as peer dependency to reduce code duplication

--- src/utils/buffer.test.ts
+++ src/utils/buffer.test.ts
@@ -1,85 +1,4 @@
-import { TextEncoder, TextDecoder } from 'util';
-import {
-  fromHex,
-  toHex,
-  numberToBuffer,
-  getTextEncoder,
-  toBuffer,
-  concat,
-  getTextDecoder,
-  toUtf8,
-  fromUtf8
-} from './buffer';
-
-describe('getTextEncoder', () => {
-  it('returns an instance of TextEncoder', () => {
-    expect(getTextEncoder()).toBeInstanceOf(TextEncoder);
-  });
-});
-
-describe('getTextDecoder', () => {
-  it('returns an instance of TextDecoder', () => {
-    expect(getTextDecoder()).toBeInstanceOf(TextDecoder);
-  });
-});
-
-describe('toHex', () => {
-  it('gets a hexadecimal string from a buffer', () => {
-    const buffer = fromHex('123456abcdef');
-    expect(toHex(buffer)).toBe('123456abcdef');
-  });
-});
-
-describe('fromHex', () => {
-  it('gets a buffer from a hexadecimal string', () => {
-    expect(fromHex('abcdef')).toBeInstanceOf(Uint8Array);
-    expect(toHex(fromHex('abcdef'))).toBe('abcdef');
-  });
-
-  it('works with and without prefixed 0x', () => {
-    const withoutPrefix = fromHex('abcdef');
-    const withPrefix = fromHex('0xabcdef');
-
-    expect(withoutPrefix).toStrictEqual(withPrefix);
-  });
-
-  it('throws if the hexadecimal string has an uneven length', () => {
-    expect(() => fromHex('123')).toThrow();
-  });
-
-  it('throws if the data has invalid characters', () => {
-    expect(() => fromHex('foobar')).toThrow();
-  });
-});
-
-describe('toBuffer', () => {
-  it('gets a buffer from a hexadecimal string', () => {
-    expect(toBuffer('abcdef')).toBeInstanceOf(Uint8Array);
-    expect(toHex(toBuffer('abcdef'))).toBe('abcdef');
-  });
-
-  it('gets a buffer from another buffer', () => {
-    expect(toBuffer(toBuffer('abcdef'))).toBeInstanceOf(Uint8Array);
-    expect(toHex(toBuffer(toBuffer('abcdef')))).toBe('abcdef');
-  });
-
-  it('gets a buffer from a number array', () => {
-    expect(toBuffer([0xab, 0xcd, 0xef])).toBeInstanceOf(Uint8Array);
-    expect(toHex(toBuffer([0xab, 0xcd, 0xef]))).toBe('abcdef');
-  });
-});
-
-describe('toUtf8', () => {
-  it('gets a string from a buffer', () => {
-    expect(toUtf8(toBuffer('666f6f626172'))).toBe('foobar');
-  });
-});
-
-describe('fromUtf8', () => {
-  it('gets a buffer from a string', () => {
-    expect(toHex(fromUtf8('foobar'))).toBe('666f6f626172');
-  });
-});
+import { numberToBuffer, toHex } from './buffer';
 
 describe('numberToBuffer', () => {
   it('writes a number to a new buffer', () => {
@@ -87,11 +6,5 @@ describe('numberToBuffer', () => {
     expect(numberToBuffer(1234, 4)).toHaveLength(4);
     expect(toHex(numberToBuffer(1234, 2))).toBe('04d2');
     expect(toHex(numberToBuffer(1234, 4))).toBe('000004d2');
-  });
-});
-
-describe('concat', () => {
-  it('concatenates multiple buffers', () => {
-    expect(toHex(concat([toBuffer('12'), toBuffer('34')]))).toBe('1234');
   });
 });
--- src/utils/buffer.ts
+++ src/utils/buffer.ts
@@ -1,106 +1,6 @@
 import getRandomValues from 'get-random-values';
 
-export type BinaryLike = string | ArrayBufferLike | number[];
-
-export const HEX_REGEX = /^[a-f0-9]+$/i;
-
-/**
- * Returns an instance of `TextEncoder` that works with both Node.js and web browsers.
- *
- * @return {TextEncoder}
- */
-export const getTextEncoder = (): TextEncoder => {
-  if (typeof TextEncoder === 'undefined') {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const Encoder = require('util').TextEncoder;
-    return new Encoder();
-  }
-
-  return new TextEncoder();
-};
-
-/**
- * Returns an instance of `TextDecoder` that works with both Node.js and web browsers.
- *
- * @return {TextDecoder}
- */
-export const getTextDecoder = (encoding = 'utf8'): TextDecoder => {
-  if (typeof TextEncoder === 'undefined') {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const Decoder = require('util').TextDecoder;
-    return new Decoder(encoding);
-  }
-
-  return new TextDecoder(encoding);
-};
-
-/**
- * Get a Uint8Array as hexadecimal string
- *
- * @param {Uint8Array} data
- * @return {string}
- */
-export const toHex = (data: Uint8Array): string => {
-  return Array.from(data)
-    .map((n) => `0${n.toString(16)}`.slice(-2))
-    .join('');
-};
-
-/**
- * Get a hexadecimal string as Uint8Array.
- *
- * @param {string} data
- * @return {Uint8Array}
- */
-export const fromHex = (data: string): Uint8Array => {
-  if (data.startsWith('0x')) {
-    data = data.slice(2);
-  }
-
-  if (data.length % 2 !== 0) {
-    throw new Error('Length must be even');
-  }
-
-  if (!data.match(HEX_REGEX)) {
-    throw new Error('Input must be hexadecimal');
-  }
-
-  return new Uint8Array(data.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16)));
-};
-
-/**
- * Attempt to parse a value as Uint8Array.
- *
- * @param {BinaryLike} data
- * @return {Uint8Array}
- */
-export const toBuffer = (data: BinaryLike): Uint8Array => {
-  if (typeof data === 'string') {
-    return fromHex(data);
-  }
-
-  return new Uint8Array(data);
-};
-
-/**
- * Get a buffer as UTF-8 encoded string.
- *
- * @param {Uint8Array} data
- * @return {string}
- */
-export const toUtf8 = (data: Uint8Array): string => {
-  return getTextDecoder().decode(data);
-};
-
-/**
- * Get a UTF-8 encoded string as buffer.
- *
- * @param {string} data
- * @return {Uint8Array}
- */
-export const fromUtf8 = (data: string): Uint8Array => {
-  return getTextEncoder().encode(data);
-};
+export { stripPrefix, toUtf8, fromUtf8, toHex, fromHex, toBuffer, concat } from '@findeth/abi';
 
 /**
  * Write a number to a Uint8Array with arbitrary length.
@@ -126,22 +26,6 @@ export const numberToBuffer = (data: number, byteLength: 1 | 2 | 4): Uint8Array 
   }
 
   return new Uint8Array(dataView.buffer);
-};
-
-/**
- * Safe function to merge multiple Uint8Arrays into a single Uint8array.
- *
- * @param {Uint8Array[]} buffers
- * @return {Uint8Array}
- */
-export const concat = (buffers: Uint8Array[]): Uint8Array => {
-  return buffers.reduce((a, b) => {
-    const buffer = new Uint8Array(a.length + b.length);
-    buffer.set(a);
-    buffer.set(b, a.length);
-
-    return buffer;
-  }, new Uint8Array(0));
 };
 
 export const getRandomBytes = (length: number): Uint8Array => {
