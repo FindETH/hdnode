db69fffd9d9dd3299982d25d888a8bdc71fd2a6f
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-03-09 00:51:48 +0100

    Add keccak function and bump @findeth/abi version

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,5 +1,7 @@
+import { BinaryLike, fromHex, toBuffer, toHex } from '@findeth/abi';
 import createHash from 'create-hash';
 import createHmac from 'create-hmac';
+import createKeccakHash from 'keccak';
 import { pbkdf2Sync } from 'pbkdf2';
 
 /**
@@ -35,6 +37,17 @@ export const ripemd160 = (buffer: Uint8Array): Uint8Array => {
 };
 
 /**
+ * Returns the Keccak-256 hash of a string, as a hexadecimal string.
+ *
+ * @param {string} input
+ * @return {string}
+ */
+export const keccak256 = (input: BinaryLike): Uint8Array => {
+  const buffer = toBuffer(input);
+  return fromHex(createKeccakHash('keccak256').update(toHex(buffer), 'hex').digest('hex'));
+};
+
+/**
  * Derive a key from a buffer and salt. Defaults to 2048 iterations, 64 byte key length and SHA512 as digest.
  *
  * @param {Uint8Array} buffer
@@ -53,5 +66,3 @@ export const pbkdf2 = (
 ): Uint8Array => {
   return pbkdf2Sync(buffer, salt, iterations, length, digest);
 };
-
-export { keccak256 } from '@findeth/abi';
